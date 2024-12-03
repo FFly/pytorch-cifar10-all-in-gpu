397d33151ae81830081e353b44980b5c24923af6
FFly <31263022+FFiot@users.noreply.github.com>
2024-12-03 21:39:47 +0800

    20241203

--- bash.py
+++ bash.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
     ]
     
     for model_name in model_list:
-        os.system(f'python train.py --log_dir=logs --mode_name={model_name}')
+        os.system(f'python train.py --log_dir=base_logs --mode_name={model_name}')
